f4feddc5e0ca3a7b590623f9932f19d9d689e4db
ErlanggaDewa <erlanggadewa7@gmail.com>
2021-04-30 08:07:35 +0700

    update

---
+++ Test/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>RKH | Rencana Kerja Harian</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+        <header class="main-header">
+            <!-- Logo -->
+            <a href="index2.html" class="logo">
+                <!-- mini logo for sidebar mini 50x50 pixels -->
+                <span class="logo-mini">RKH</span>
+                <!-- logo for regular state and mobile devices -->
+                <span class="logo-lg">Rencana Kerja Harian</span>
+            </a>
---
+++ Test/index.php
@@ -0,0 +1,27 @@
+<?php include('./header.php') ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Dashboard
+            <small>Sistem Rencana Harian Kerja</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="callout callout-info">
+            <p>Selamat datang di Sistem Rencana Harian Kerja.</p>
+            <p>Gunakan sistem ini mencatat setiap permasalah yang dihadapi setiap hari.
+                <br />Caranya ..... , lalu .....
+                <br />Update .....
+            </p>
+        </div>
+
+    </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+<?php include('./footer.php') ?>
